c715993add48074172444a371e4260ce92574ef3
Totti26 <93136994+Totti26@users.noreply.github.com>
2022-02-11 21:52:05 -0600

    Update data.py

--- data.py
+++ data.py
@@ -10,7 +10,10 @@ class stationInfo():
         self.info = {
             "Max" : [],
             "Min" : [],
-            
+            "Humidity" : [],
+            "WindMax" : [],
+            "WindMin" : [],
+            "Rain" : [],
         }
         self.months = []
 
@@ -42,20 +45,46 @@ class stationInfo():
                     Min = data
         return Min
 
-    def printInfo(self):
-        print(f"The highest temp for {self.name} station was {round(self.maxTemp, 2)}. ")
-        print(f"The lowest temp for {self.name} station was {round(self.maxTemp, 2)}. ")
-        print(f"The average highest temp for {self.name} station was {round(self.averageMax, 2)}. ")
-        print(f"The average lowest temp for {self.name} station was {round(self.averageMin, 2)}. ")
-        print("")
+    def plotInfo(self, list):
+        plot.plot(range(len(list)), list, label = station.name)
+
+    def cleanInfo(self, infoName):
+        newList = []
+        for i in range(len(self.info[infoName])):
+            if self.info[infoName][i] == "":
+                forwardStep = 1
+                while self.info[infoName][i + forwardStep] == "":
+                    forwardStep += 1
+                if i == 0:
+                    info = self.info[infoName][i + forwardStep]
+                elif i == len(self.info[infoName]) - 1:
+                    info = newList[i - 1]
+                else:
+                    info = ((self.info[infoName][i + forwardStep] - info) / forwardStep + 1) + info
+            else:
+                info = self.info[infoName][i]
+            newList.append(info)
+        return newList
+
+    def findMonth(self,month, list):
+        newList = []
+        g = 0
+        for i in list:
+            if int(self.months[g]) == int(month):
+                newList.append(i)
+            g += 1
+        return newList
 
 with open('2016VizData.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         max = float(row["TMAX"])
         min = float(row["TMIN"])
+        humidity = float(row["HAVG"])
+        windMax = float(row["WSMX"])
+        windMin = float(row["WSMN"])
+        rain = float(row["RAIN"])
         
-
         currentStation = row["STID"]
         i = 0
         if len(stations) == 0:
@@ -71,13 +100,29 @@ with open('2016VizData.csv', newline='') as csvfile:
             if station.name == currentStation:
                 station.addData("Max", max, (-80, 120))
                 station.addData("Min", min, (-80, 120))
-                
-name = input('Hello, what is your name? ')
-print(f'hello {name}')
+                station.addData("Humidity", humidity, (-80, 120))
+                station.addData("WindMax", windMax, (-80, 120))
+                station.addData("WindMin", windMin, (-80, 120))
+                station.addData("Rain", rain, (-80, 120))
+                station.months.append(row["MONTH"])
+
+name = input('Hello! What is your name? ')
+print(f'Hello {name}!')
 while True:
+    print(f'What weather data would you like to see? ')
+    data = input('Max, Min, Humidity, WindMax, WindMin, Rain: ')
+    time = input(f'For what time would you like to see that info (month #, year): ')
+
     for station in stations:
-        stationInfo()
-    
+        dataList = station.cleanInfo(data)
+        try:
+            if int(time) in range(11):
+                newDataList = station.findMonth(int(time) + 1, dataList)
+                station.plotInfo(newDataList)
+            else:
+                station.plotInfo(dataList)
+        except:
+            station.plotInfo(dataList)
                 
     plot.legend()
     plot.show()
